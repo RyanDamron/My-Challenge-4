837ee8d097f9b8443a28bdd8a917e890237c19aa
RyanDamron <119913449+RyanDamron@users.noreply.github.com>
2023-01-09 18:40:14 -0600

    finished for now

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var startEl = document.querySelector("#start")
 var startMessage = document.querySelector("#start-message")
 var hideQuestion = document.querySelector("#question")
 var timeLeft = 60;
+var questionNumber = 0;
+
 function hiddenQuestion() {
     document.getElementById('question').style.visibility = "hidden";
 
@@ -11,7 +13,7 @@ hiddenQuestion();
 startEl.addEventListener("click", function (event) {
     event.preventDefault();
     startMessage.textContent = ('');
-    showQuestion();
+    showQuestionOne();
     document.getElementById('question').style.visibility = "visible";
     var timerEl = document.getElementById('countdown');
 
@@ -36,45 +38,54 @@ startEl.addEventListener("click", function (event) {
 
 })
 
-var questionBank = [
-    {
+function loseTime () {
+    timeLeft = timeLeft -9;
+}
+
+var questionBank = [{
+    
         question: "What is the name of Smalls' stepdad?",
         answers: ["Adam", "Bill", "Charles", "Doug"],
         correct: "Bill"
-    },
+},
+// var questionBankTwo = {
     {
         question: "What song is playing when the boys are on the carnival ride?",
         answers: ["Twist and Shout", "Tossing and Turning", "The Twist", "Tequila"],
         correct: "Tequila"
     },
+// var questionBankThree = 
     {
         question: "According to Benny, what does Yeah-Yeah run like?",
         answers: ["a Duck", "a Horse", "a Cheetah", "a Turtle"],
         correct: "a Duck"
     },
+// var questionBankFour =
     {
         question: "Which is NOT one of Babe Ruth's nicknames?",
         answers: ["The Great Bambino", "The Colossus of Clout", "Baby Ruthie", "The Sultan of Swat"],
         correct: "Baby Ruthie"
     },
+// var questionBankFive =
     {
         question: "How many guys does Squints say The Beast ate while he was a guard dog?",
         answers: ["120", "185", "173", "150"],
         correct: "173"
-    },
-
+    }
 ]
-console.log(questionBank[0].question);
-
+var removeButton = document.getElementsByClassName('button');
 var currentQuestion = 0;
-function showQuestion() {
-    
-    
-    for (var i = 0; i < questionBank.length; i++){
+
+function showQuestionOne() {
+        console.log(questionNumber);
+        if (questionNumber < 5) {
+
+        
         var stimulus = document.getElementById('question-text-1');
-        stimulus.textContent = questionBank[currentQuestion].question;
-        var correctAnswer = questionBank[currentQuestion].correct;
-        console.log('Correct Answer:' + correctAnswer);
+        stimulus.innerHTML = ""
+        stimulus.textContent = questionBank[questionNumber].question;
+        var correctAnswer = questionBank[questionNumber].correct;
+        // console.log('Correct Answer:' + correctAnswer);
         var wrongNote = document.createElement("h4");
         var rightNote = document.createElement("h4");
     
@@ -83,58 +94,104 @@ function showQuestion() {
         stimulus.append(rightNote);
         rightNote.textContent = ('');
     
-    for (var i = 0; i < questionBank[currentQuestion].answers.length; i++) {
-        console.log(questionBank[currentQuestion].answers[i]);
+    for (var i = 0; i < questionBank[questionNumber].answers.length; i++) {
+        // console.log(questionBank[0].answers[i]);
         var button = document.createElement("button");
         button.setAttribute("class", "input");
         button.setAttribute("class", "button");
         button.style.display = "block";
-        button.textContent = questionBank[currentQuestion].answers[i];
-        stimulus.append(button);
-    }
-        
+        button.textContent = questionBank[questionNumber].answers[i];
 
-    
-        stimulus.addEventListener('click', function (event) {
+        button.addEventListener('click', function (event) {
             event.preventDefault();
             var userChoice = event.target.textContent;
             console.log('userchoice:' + userChoice);
             if (userChoice !== correctAnswer) {
                 wrongNote.textContent = "You're Killing Me, Smalls!"
-                timeLeft -= 15;
+                loseTime();
             } else {
                 rightNote.textContent = "Heros Get Remembered, But Legends Never Die!"
+                timeLeft = timeLeft + 1
             } 
-           
-            // this moved to a new question
+            questionNumber++;
+            setTimeout(showQuestionOne, 1000);     
             
-
-        })
-        questionBank.question ++;
-        questionBank.correct ++;
-        questionBank.answers ++;
-        
+        })        
+        stimulus.append(button);
 
         
+    } 
+}else{
+    initialsPage();
+}
+
+}              
+
+let initialsPage = () =>{
+    document.getElementById('question-text-1').innerHTML=""
+    let scorePara = document.createElement("p");
+    scorePara.setAttribute("class", "questions");
+    if (timeLeft < 0){
+        timeLeft = 0
     }
-    return;
-    // showQuestion();     
-}   
-            
-
-    
+    scorePara.innerHTML = "Your score is " + timeLeft + "!";
     
 
+    let initialsDiv = document.createElement("div");
+    initialsDiv.style.display= "flex"
+    initialsDiv.setAttribute("id", "question");
+    let initialsInput = document.createElement("input");
+    initialsInput.setAttribute("id", "initials");
+    initialsInput.placeholder = "Enter your initials!"
+    let submitBtn = document.createElement("button");
+    submitBtn.setAttribute("class", "input");
+    submitBtn.setAttribute("class", "button");
+    submitBtn.style.display = "block"
+    submitBtn.textContent = "Submit"
+    submitBtn.addEventListener("click", function(event){
+        event.preventDefault();
+        let inputValue = document.getElementById("initials").value;
+        console.log(inputValue);
+        saveToLocalStorage(inputValue, timeLeft);
+        console.log(initialsInput);
+        
+    }) 
+    scorePara.appendChild(initialsInput);
+    scorePara.appendChild(submitBtn);
+    document.getElementById('question-text-1').appendChild(scorePara, initialsDiv);
+
+}
+function saveToLocalStorage(initials, score){
+    let localStorageData = JSON.parse(localStorage.getItem("quizScore"));
+    let data = {
+        initials: initials,
+        score: score
+    }
+    if (localStorageData === null){
+        localStorageData = [];
+        localStorageData.push(data)
+    }else{
+        localStorageData.push(data)
+    }
+    localStorage.setItem("quizScore", JSON.stringify(localStorageData))
+    finalPage();
+}
+
+function finalPage (){
+    alert("Thank you for playing!")
+    // window.location.href = 'scores.html';
+    loadHighScores ();
+}
+function loadHighScores () {
+    let localStorageData = JSON.parse(localStorage.getItem("quizScore"));
+    console.log(localStorageData);
+    for (var i = 0; i < localStorageData.length; i++) {
+        var newLi = document.createElement('li');
+        var scoreList = document.getElementById("score-list");
+        newLi.textContent = "Initials: " + localStorageData[i].initials + " Score " +   localStorageData[i].score;
+        scoreList.appendChild(newLi);
+    }
+}
 
 
-    
-
-
-    //create input elements in JS
-    //append them to the page
-    //set text content using for loop
-
-
-
-
-
+//window.location.href = 'highscores.html';
